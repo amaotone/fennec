3ef7dc17479a4b68477b51b07df3b28a8e3cf29e
Amane <amaotone@users.noreply.github.com>
2016-06-17 19:41:30 +0900

    Add python 2 compatibility (#2)
    Add python 2 compatibility to workspace.py

--- fennec/workspace.py
+++ fennec/workspace.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import logging
 import os
 import re
@@ -7,41 +9,56 @@ from pytz import timezone
 
 
 class Workspace(object):
-    """Workspace manager class."""
+    """Workspace manager.
+
+    Args:
+        name (str): Workspace name. (it is used for directory naming)
+        root (str): Root of results.
+
+    Attributes:
+        name (str): Workspace name.
+        category (str): Workspace category.
+    """
 
     root = "./results"
-    time_zone = timezone("Asia/Tokyo")
-    datetime_fmt = "%Y%m%d_%H%M%S"
+    zone = timezone("Asia/Tokyo")
+    date_fmt = "%Y%m%d_%H%M%S"
 
     def __init__(self,
                  name="experiment",
                  root="./results"):
         self.category, self.name = re.match(r"(?:(.+)/)?(.+)", name).groups("")
         Workspace.root = root
-        self.created = datetime.now(Workspace.time_zone)
-        self.path = os.path.join(Workspace.root,
-                                 self.category,
-                                 "{}_{}".format(self.created.strftime(Workspace.datetime_fmt), self.name))
-        self.logger = None
+        self._created = datetime.now(Workspace.zone)
+        self._path = os.path.join(Workspace.root,
+                                  self.category,
+                                  "{}_{}".format(self._created.strftime(Workspace.date_fmt), self.name))
+        self._logger = None
 
-    def log(self, message):
+    def log(self, message, display=True):
         """Logging"""
-        if self.logger is None:
+        if self._logger is None:
             self._init_logger()
 
-        self.logger.info(message)
+        self._logger.info(message)
+
+        if display:
+            print(message)
 
     def _init_logger(self):
         logging.basicConfig(filename=self._get_abs_path("result.log"),
                             format="[%(asctime)s %(name)s] %(message)s",
                             datefmt="%Y/%m/%d %I:%M:%S",
                             level=logging.DEBUG)
-        self.logger = logging.getLogger(self.name)
+        self._logger = logging.getLogger(self.name)
 
     def _get_abs_path(self, relpath=""):
         """Convert relative path to absolute path based on workspace directory."""
-        abspath = os.path.abspath(os.path.join(self.path, relpath))
-        os.makedirs(os.path.dirname(abspath), exist_ok=True)
+        abspath = os.path.abspath(os.path.join(self._path, relpath))
+
+        # exist_ok does not exists for os.makedirs in python 2
+        if not os.path.exists(abspath):
+            os.makedirs(os.path.dirname(abspath))
         return abspath
 
     def __call__(self, relpath=""):
